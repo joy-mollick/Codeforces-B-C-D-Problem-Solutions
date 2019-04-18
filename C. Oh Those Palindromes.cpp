c42c669b76fb7c311513b3fac6067c426f29e3e2
Nayeem Mollick Joy <nayeemjoy59@gmail.com>
2019-04-18 13:02:20 -0700

    Add files via upload

---
+++ C. Oh Those Palindromes.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+    long a,b,c;
+    int t;
+    cin>>t;
+    string s;
+    cin>>s;
+    sort(s.begin(),s.end());
+    cout<<s<<endl;
+    return 0;
+}
